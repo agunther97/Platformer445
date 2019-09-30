074d30ca40c6c3f07d1efd8e4b016fef57ff20d8
Christian <c.ferguson93@gmail.com>
2019-09-30 17:18:15 -0500

    Foundation of the game
    Basic movement and grappling mechanics

---
+++ Platformer445/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animate;
+
+    private PlayerController pc;
+
+
+    void Start(){
+        animate = GetComponent<Animator>();
+        pc = GetComponent<PlayerController>();
+    }
+
+    void Update(){
+        AnimateControls();   
+    }
+
+    void AnimateControls(){
+        //Animate Running
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+            animate.SetBool("isRunning", true);
+        } else{
+            animate.SetBool("isRunning", false);
+        }
+
+        //Animate Jumping
+        if(Input.GetKeyDown(KeyCode.Space)){
+            animate.SetTrigger("isJumping");
+        }
+
+        //Animate Falling
+        if(pc.GetRigidbody().velocity.y < -0.01){
+            animate.SetBool("isFalling", true);
+        } else{
+            animate.SetBool("isFalling", false);
+        }
+    }
+}
---
+++ Platformer445/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Character movement attributes
+    public float speed;
+    public float jumpForce = 6f;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    //GroundCheck controls
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+      Jump();
+    }
+
+    void FixedUpdate(){
+      Movement();
+      
+      if(facingRight == false && (moveInput > 0)){
+        Flip();
+      } else if(facingRight == true && moveInput < 0){
+          Flip();
+      }
+    }
+
+    //Character movement
+    void Movement(){
+      moveInput = Input.GetAxisRaw("Horizontal");
+      rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    //Character jump
+    void Jump(){
+      //Determines if player is on ground
+      isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+      if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true){
+        rb.velocity = Vector2.up * jumpForce;
+      }
+    }
+
+    //Keeps sprite facing in the direction player is moving towards
+    void Flip(){
+      facingRight = !facingRight;
+      Vector3 Scaler = transform.localScale;
+      Scaler.x *= -1;
+      transform.localScale = Scaler;
+    }
+
+    public Rigidbody2D GetRigidbody(){
+      return rb;
+    }
+}
