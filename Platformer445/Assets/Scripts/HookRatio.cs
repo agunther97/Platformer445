f8131a4290dfe41f748467adcdeafba123a752c7
Christian <c.ferguson93@gmail.com>
2019-10-16 13:00:12 -0500

    Level 2 and Moving Platforms
    Adds Level2(Ozy) and MovingPLatforms(Christian)

---
+++ Platformer445/Assets/Scripts/HookRatio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookRatio : MonoBehaviour
+{
+
+    public GameObject player;
+    public Vector3 grabPosition;
+
+    public float ratio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scaleX = Vector3.Distance(player.transform.position, grabPosition)/ratio;
+        GetComponent<LineRenderer>().material.mainTextureScale = new Vector2(scaleX, 1f);
+    }
+}
