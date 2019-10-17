1b9df0dc4c9b59d09d5d9acbb32461ef728ae06a
Christian <c.ferguson93@gmail.com>
2019-10-16 21:08:57 -0500

    Added PlayerDeath script and DeathSpace
    Added the playerdeath script, and the deathspace mechanic

---
+++ Platformer445/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int livesLeft = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();
+    }
+
+    private void GameOver(){
+        if(livesLeft == 0){
+            Debug.Log("GameOver");
+        }
+    }
+}
---
+++ Platformer445/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public Transform spawnPos;
+
+    //Stores  object
+    GameObject gO;
+    //Calls GameManager script
+    GameManager gM;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitPlayerDeathChecks();
+    }
+
+    public void InitPlayerDeathChecks()
+    {
+        //Stores GameManager object to gO
+        gO= GameObject.Find("GameManager");
+        //Calls GameManager script from GameManager object
+        gM = gO.GetComponent<GameManager>();
+    }
+
+    void OnCollisionEnter2D(Collision2D theCollsion)
+    {
+        if(theCollsion.gameObject.tag == "Player"){
+            Debug.Log("Sub1");
+            theCollsion.transform.position = spawnPos.position;
+            gM.livesLeft--;
+        }
+    }
+}
