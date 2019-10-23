0bc4c79a562f4dea43c9cdb4329a9adf7932afca
Osvaldo Sanchez <osvsanchez1@gmail.com>
2019-10-23 14:35:43 -0500

    Level 2 Polish + Collectibles
    Made Level 2 Longer
    Play tested
    Polished tiles so they're consistent
    Added collectible script
    Added score script
    Implemented collectibles into both levels

---
+++ Platformer445/Assets/Scripts/Collectible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    [SerializeField]
+    Canvas messageCanvas;
+
+    void Start()
+    {
+        messageCanvas.enabled = false;
+        ScoreScript.flowersPresent++;
+    }
+
+    // When a player enters the collider for the flower, it enables the collected image, destroys the flower, and raises the score
+    void OnTriggerEnter2D(Collider2D theCollision)
+    {
+        if (theCollision.gameObject.tag == "Player")
+        {
+            ScoreScript.flowersCollected++;
+            messageCanvas.enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
